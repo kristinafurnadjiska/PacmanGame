0ade8c4fcdccd4b7fcc6753962838a6d1530e7f6
Kristina Furnadjiska <furnadjiska.k@gmail.com>
2022-07-26 16:26:59 +0200

    Adding remaining classes

---
+++ PacmanGame/Items/GameConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanGame.Items
+{
+    public enum Level { Easy, Medium, Hard, Advanced}
+    public class GameConfig
+    {
+        public Level Level { get; set; }
+        public int NumberOfEnemeies { get; set; }
+        public int NumberOfPortals { get; set; }
+    }
+}
