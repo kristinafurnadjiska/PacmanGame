56eb39fbfc961afec1881c4a6c4cc59bb48b2fc5
Kristina Furnadjiska <furnadjiska.k@gmail.com>
2022-08-30 11:30:45 +0200

    Max limit for portals

--- PacmanGame/Screens/Configuration.Designer.cs
+++ PacmanGame/Screens/Configuration.Designer.cs
@@ -47,9 +47,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(55, 41);
+            this.pictureBox1.Location = new System.Drawing.Point(41, 33);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(348, 126);
+            this.pictureBox1.Size = new System.Drawing.Size(261, 102);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -68,22 +69,24 @@
             "Easy",
             "Medium",
             "Hard"});
-            this.cbLevel.Location = new System.Drawing.Point(53, 207);
+            this.cbLevel.Location = new System.Drawing.Point(40, 168);
+            this.cbLevel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbLevel.Name = "cbLevel";
-            this.cbLevel.Size = new System.Drawing.Size(250, 25);
+            this.cbLevel.Size = new System.Drawing.Size(188, 21);
             this.cbLevel.TabIndex = 7;
             this.cbLevel.SelectedIndexChanged += new System.EventHandler(this.cbLevel_SelectedIndexChanged);
             // 
             // nudEnemie
             // 
-            this.nudEnemie.Location = new System.Drawing.Point(53, 266);
+            this.nudEnemie.Location = new System.Drawing.Point(40, 216);
+            this.nudEnemie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nudEnemie.Maximum = new decimal(new int[] {
             4,
             0,
             0,
             0});
             this.nudEnemie.Name = "nudEnemie";
-            this.nudEnemie.Size = new System.Drawing.Size(250, 22);
+            this.nudEnemie.Size = new System.Drawing.Size(188, 20);
             this.nudEnemie.TabIndex = 8;
             // 
             // nudPortals
@@ -93,23 +96,25 @@
             0,
             0,
             0});
-            this.nudPortals.Location = new System.Drawing.Point(53, 317);
+            this.nudPortals.Location = new System.Drawing.Point(40, 258);
+            this.nudPortals.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nudPortals.Maximum = new decimal(new int[] {
-            10,
+            4,
             0,
             0,
             0});
             this.nudPortals.Name = "nudPortals";
-            this.nudPortals.Size = new System.Drawing.Size(250, 22);
+            this.nudPortals.Size = new System.Drawing.Size(188, 20);
             this.nudPortals.TabIndex = 9;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Snap ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(52, 181);
+            this.label1.Location = new System.Drawing.Point(39, 147);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(238, 23);
+            this.label1.Size = new System.Drawing.Size(195, 18);
             this.label1.TabIndex = 10;
             this.label1.Text = "Select difficulty level ";
             // 
@@ -117,9 +122,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Snap ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(52, 236);
+            this.label2.Location = new System.Drawing.Point(39, 192);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(258, 23);
+            this.label2.Size = new System.Drawing.Size(214, 18);
             this.label2.TabIndex = 11;
             this.label2.Text = "Select number of enemies";
             // 
@@ -127,9 +133,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Snap ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(52, 291);
+            this.label3.Location = new System.Drawing.Point(39, 236);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(251, 23);
+            this.label3.Size = new System.Drawing.Size(208, 18);
             this.label3.TabIndex = 12;
             this.label3.Text = "Select number of portals";
             // 
@@ -138,9 +145,10 @@
             this.btnBack.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnBack.Font = new System.Drawing.Font("Snap ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBack.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnBack.Location = new System.Drawing.Point(285, 367);
+            this.btnBack.Location = new System.Drawing.Point(214, 298);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(118, 37);
+            this.btnBack.Size = new System.Drawing.Size(88, 30);
             this.btnBack.TabIndex = 13;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = false;
@@ -148,10 +156,10 @@
             // 
             // Configuration
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(443, 446);
+            this.ClientSize = new System.Drawing.Size(332, 362);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -160,6 +168,7 @@
             this.Controls.Add(this.nudEnemie);
             this.Controls.Add(this.cbLevel);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Configuration";
             this.Text = "Configuration";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
